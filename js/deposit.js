94cb257207c26e5e45836428e3426224b39c2a33
rifat3790 <mdrifayethossen@gmail.com>
2023-08-15 20:45:49 +0600

    added deposit and wothdraw with using javascript

--- js/deposit.js
+++ js/deposit.js
@@ -15,12 +15,24 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
 
     const currentDepositTotal = previousDepositTotal + newDepositAmount;
 
+    // Get display balance field starts
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalAmountString = balanceTotal.innerText;
+    const balanceTotalAmount = parseFloat (balanceTotalAmountString);
+
+    const currentBalance = balanceTotalAmount + newDepositAmount;
+    // End
+
     // depositTotal.innerText = currentDepositTotal;
     if(isNaN(newDepositAmount)){
         return alert("This is not a Number. Please enter valid Number.")
     }
     else if(newDepositAmount < 0){
         return alert("Negative amount deposit is not possible. Please enter positive amount.")
+    }
+    else{
+        depositTotal.innerText = currentDepositTotal;
+        balanceTotal.innerText = currentBalance;
     }
 
     depositFiled.value = '';
---
+++ js/withdraw.js
@@ -0,0 +1,46 @@
+document.getElementById('btn-withdraw').addEventListener('click', function(){
+
+    // Get input field for withdraw starts
+    const withdrawTotal = document.getElementById('withdraw-field');
+    const newWithdrawTotalAmountString = withdrawTotal.value;
+    const newWithdrawTotalAmount = parseFloat (newWithdrawTotalAmountString);
+    // console.log(newWithdrawTotalAmount)
+    // End
+
+    // Get Display withdraw amount 
+    const displayWithdraw = document.getElementById('withdraw-total');
+    const previousWitdrawTotalString = displayWithdraw.innerText;
+    const previousWitdrawTotal = parseFloat(previousWitdrawTotalString);
+    // console.log(typeof previousWitdrawTotal);
+    // End
+
+    const currentWithdraw = newWithdrawTotalAmount + previousWitdrawTotal;
+
+    // Get Display balance amount field
+    const balanceTotalField = document.getElementById('balance-total');
+    const balanceAmountString = balanceTotalField.innerText;
+    const balanceAmount = parseFloat(balanceAmountString);
+
+    const currentBalanceAmount = balanceAmount - newWithdrawTotalAmount;
+    
+
+    if(isNaN(newWithdrawTotalAmount)){
+        return alert("This is not a Number. Please enter valid Number.")
+    }
+    else if (newWithdrawTotalAmount < 0){
+        return alert("Negative withdraw amount is not possible. Please enter positive withdraw amount.")
+    }
+    else if(newWithdrawTotalAmount > currentBalanceAmount){
+        return alert("Insufficient balance. Please waiting for deposit.")
+    }
+    else{
+        displayWithdraw.innerText = currentWithdraw;
+        balanceTotalField.innerText = currentBalanceAmount;
+    }
+
+
+
+
+    withdrawTotal.value = '';
+
+})
